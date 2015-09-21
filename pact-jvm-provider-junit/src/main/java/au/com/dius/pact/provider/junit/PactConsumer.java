6122729b35fa915a0cc49f2110afbbeb31acfac8
Philipp Berger <pbe@zuehlke.com>
2015-09-22 01:04:52 +0200

    Merged PactConsumer and PactRule

--- pact-jvm-provider-junit/src/main/java/au/com/dius/pact/provider/junit/PactConsumer.java
+++ pact-jvm-provider-junit/src/main/java/au/com/dius/pact/provider/junit/PactConsumer.java
@@ -2,6 +2,9 @@ package au.com.dius.pact.provider.junit;
 
 import au.com.dius.pact.model.*;
 import au.com.dius.pact.model.dispatch.HttpClient;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
 import scala.Option;
 import scala.concurrent.Await;
 import scala.concurrent.ExecutionContext$;
@@ -12,6 +15,7 @@ import scala.concurrent.duration.Duration;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
 import java.util.NoSuchElementException;
 import java.util.concurrent.Executors;
@@ -19,16 +23,18 @@ import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
-public class PactConsumer {
+public class PactConsumer implements TestRule {
 
-    private static au.com.dius.pact.model.Pact currentPact;
-    private static Interaction interaction;
-    private static Future<Response> actualResponseFuture;
 
-    private PactConsumer() {
+    private au.com.dius.pact.model.Pact currentPact;
+    private Interaction interaction;
+    private Future<Response> actualResponseFuture;
+
+    public PactConsumer(String pactFile) {
+        currentPact = Pact.from(readFileContents(pactFile));
     }
 
-    public static void verifyPact() {
+    public void verifyPactInteraction() {
         Response actualResponse = null;
         try {
             actualResponse = Await.result(actualResponseFuture, Duration.create(1000, TimeUnit.SECONDS));
@@ -38,24 +44,11 @@ public class PactConsumer {
         assertEquals(FullResponseMatch$.MODULE$, ResponseMatching$.MODULE$.matchRules(interaction.response(), actualResponse));
     }
 
-    static void setCurrentPact(String pactFile, String interactionDescription, String providerState) {
-        currentPact = au.com.dius.pact.model.Pact.from(readFileContents(pactFile));
-        try {
-            interaction = currentPact.interactionFor(interactionDescription, Option.<String>apply(providerState)).get();
-        } catch (NoSuchElementException e) {
-            throw new IllegalArgumentException("Cannot find interaction with description " + interactionDescription + " and provider state " + providerState);
-        }
-    }
-
-    public static void sendRequest(String host, int port) {
-        testPact(interaction, host, port);
-    }
-
-    private static void testPact(Interaction interaction, String host, int port) {
+    public void sendRequest(String host, int port) {
         try {
             final ExecutionContextExecutor executionContextExecutor = ExecutionContext$.MODULE$.fromExecutor(Executors.newCachedThreadPool());
             final Request request = new Request(interaction.request().method(),
-                    "http://"+host+":"+port + interaction.request().path(),
+                    "http://"+ host +":"+ port + interaction.request().path(),
                     interaction.request().query(),
                     interaction.request().headers(),
                     interaction.request().body(),
@@ -66,7 +59,15 @@ public class PactConsumer {
         }
     }
 
-    private static String readFileContents(String path) {
+    void setCurrentPactInteraction(String interactionDescription, String providerState) {
+        try {
+            interaction = currentPact.interactionFor(interactionDescription, Option.<String>apply(providerState)).get();
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("Cannot find interaction with description " + interactionDescription + " and provider state " + providerState);
+        }
+    }
+
+    private String readFileContents(String path) {
         try (final InputStream in = PactRunner.class.getResourceAsStream(path);
              ByteArrayOutputStream out = new ByteArrayOutputStream()) {
             byte[] buffer = new byte[8192];
@@ -77,6 +78,46 @@ public class PactConsumer {
             return out.toString(StandardCharsets.UTF_8.name());
         } catch (IOException ex) {
             throw new RuntimeException("Could not read pact file: " + path);
+        }
+    }
+
+    @Override
+    public Statement apply(Statement statement, Description description) {
+        return new PactWrappedStatement(statement, description);
+    }
+
+    private class PactWrappedStatement extends Statement {
+
+        private Statement wrappedStatement;
+        private Description description;
+
+        public PactWrappedStatement(Statement wrappedStatement, Description description) {
+            this.wrappedStatement = wrappedStatement;
+            this.description = description;
+        }
+
+        @Override
+        public void evaluate() throws Throwable {
+            PactFile annotation = description.getTestClass().getAnnotation(PactFile.class);
+            if (annotation == null) {
+                throw new IllegalArgumentException("Test class must be annotated with @" + PactFile.class.getName());
+            }
+            String pactFile = annotation.value();
+
+            Method testMethod = description.getTestClass().getMethod(description.getMethodName());
+            InteractionDescription providerStateAnnotation = testMethod.getAnnotation(InteractionDescription .class);
+            if (providerStateAnnotation == null) {
+                throw new IllegalStateException("Test method must be annotated with @" + InteractionDescription .class.getName());
+            }
+            String interactionDescription = providerStateAnnotation.description();
+            String providerState = providerStateAnnotation.providerState();
+            if (providerState.isEmpty()) {
+                providerState = null;
+            }
+
+            setCurrentPactInteraction(interactionDescription, providerState);
+
+            wrappedStatement.evaluate();
         }
     }
 }
--- pact-jvm-provider-junit/src/test/java/au/com/dius/pact/provider/junit/PactRuleTest.java
+++ pact-jvm-provider-junit/src/test/java/au/com/dius/pact/provider/junit/PactRuleTest.java
@@ -3,6 +3,7 @@ package au.com.dius.pact.provider.junit;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -11,27 +12,31 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 
-@PactFile("/exampleSpec.json")
 public class PactRuleTest {
 
     @Rule
-    public PactRule pactRule = new PactRule();
+    public PactConsumer pactConsumer = new PactConsumer("/exampleSpec.json");
 
     @Test
     @InteractionDescription(description = "test interaction", providerState = "test state")
     public void testState() throws IOException {
         // given
-
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/", new SuccessHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
 
         // when
-        PactConsumer.sendRequest("localhost", 8080);
+        pactConsumer.sendRequest("localhost", 8080);
 
         // then
-        PactConsumer.verifyPact();
+        pactConsumer.verifyPactInteraction();
+    }
+
+    @Test
+    @Ignore
+    public void otherTest() {
+
     }
 
 
